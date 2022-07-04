803abc36e8aea7477d637c08f0a8d6823a035e18
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-04 10:07:31 +0200

    :white_check_mark: test:make UserFixtures and load it with success

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public const FAKEUSERS = 20;
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < self::FAKEUSERS; $i++) {
+            $user = new User();
+            $user->setEmail('jane' . $i . '@mail.com');
+            $user->setPassword('paSSword!' . $i);
+            $user->setUsername('jane' . $i);
+            $user->setCreatedAt(new DateTimeImmutable());
+            $manager->persist($user);
+        }
+        //insert all users in DB at the same time
+        $manager->flush();
+    }
+}
