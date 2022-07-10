6ee7adefdcc91be9b8d8bbfbd5247e1bbf1b8ff9
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-10 13:26:57 +0200

    :sparkles: feat:add assertions to SeedBatch Entity

--- src/Entity/SeedBatch.php
+++ src/Entity/SeedBatch.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\SeedBatchRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: SeedBatchRepository::class)]
 class SeedBatch
@@ -11,26 +12,43 @@ class SeedBatch
     //to update min/max quantities, just update thoses constants, use everywhere even in fixtures
     public const MINSEEDSQUANTITY = 5;
     public const MAXSEEDSQUANTITY = 25;
+    public const MAXBATCHADDED = 10;
+    public const MINBATCHADDED = 1;
 
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private $id;
+    private int $id;
 
     #[ORM\Column(type: 'integer')]
-    private $seedQuantity;
+    #[Assert\NotBlank(
+        message: 'Vous devez renseigner une quantité'
+    )]
+    #[Assert\Type(
+        type: 'integer',
+        message: '{{ value }} n\'est pas un nombre entier.',
+    )]
+    #[Assert\LessThanOrEqual(
+        value: self::MAXSEEDSQUANTITY,
+        message:'La quantité de graines est trop élevée'
+    )]
+    #[Assert\GreaterThanOrEqual(
+        value: self::MINSEEDSQUANTITY,
+        message:'La quantité de graines est trop faible'
+    )]
+    private int $seedQuantity;
 
     #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'seedBatches')]
     #[ORM\JoinColumn(nullable: false)]
-    private $owner;
+    private User $owner;
 
     #[ORM\ManyToOne(targetEntity: Plant::class, inversedBy: 'seedBatches')]
     #[ORM\JoinColumn(nullable: false)]
-    private $plant;
+    private Plant $plant;
 
     #[ORM\ManyToOne(targetEntity: Quality::class, inversedBy: 'seedBatches')]
     #[ORM\JoinColumn(nullable: false)]
-    private $quality;
+    private Quality $quality;
 
     public function getId(): ?int
     {
