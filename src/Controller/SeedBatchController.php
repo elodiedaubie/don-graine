b557c4f1962d8bcb058f28bdc01154b5a9d68f2d
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-10 13:12:38 +0200

    :sparkles: feat:securize route to logged users

--- src/Controller/SeedBatchController.php
+++ src/Controller/SeedBatchController.php
@@ -5,12 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
+#[IsGranted('ROLE_USER')]
+#[Route('/donner', name: 'seed_batch')]
 class SeedBatchController extends AbstractController
 {
-    #[Route('/donner', name: 'seed_batch')]
+    #[Route('', name: '_add')]
     public function index(): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        
         return $this->render('seed_batch/index.html.twig', [
             'controller_name' => 'SeedBatchController',
         ]);
