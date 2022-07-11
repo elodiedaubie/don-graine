fe0b22d5cfd50741ba0a77d89575ad1d81e1c5fe
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-11 11:47:19 +0200

    :sparkles: feat:add picto property to Purpose entity and fixtures

--- src/DataFixtures/PurposeFixtures.php
+++ src/DataFixtures/PurposeFixtures.php
@@ -11,16 +11,17 @@ class PurposeFixtures extends Fixture
     //this constant is meant to be uses in fixtures classes
     //to add a new purpose, just add it on the const, it will be load in other fixtures
     public const PURPOSESFIXTURES = [
-       'Vegetable',
-       'Melliferous',
-       'Decorative',
+       ['Potagère', 'picto-vegetable.png'],
+       ['Mellifère', 'picto-melliferous.png'],
+       ['Décorative', 'picto-decorative.png']
     ];
 
     public function load(ObjectManager $manager): void
     {
-        foreach (self::PURPOSESFIXTURES as $key => $name) {
+        foreach (self::PURPOSESFIXTURES as $key => $infos) {
             $purpose = new Purpose();
-            $purpose->setName($name);
+            $purpose->setName($infos[0]);
+            $purpose->setPicto($infos[1]);
             $manager->persist(($purpose));
             $this->addReference('purpose_' . $key, $purpose);
         }
