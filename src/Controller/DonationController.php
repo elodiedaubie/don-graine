5cd42bbb8ec18d0f16592d2d772cab7ceabf9be3
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-12 15:57:16 +0200

    :sparkles: feat:complete addDonation function

--- src/Controller/DonationController.php
+++ src/Controller/DonationController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use DateTimeImmutable;
 use App\Entity\Donation;
 use App\Entity\SeedBatch;
-use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[IsGranted('ROLE_USER')]
 #[Route('/donation', name: 'donation')]
@@ -18,52 +20,71 @@ class DonationController extends AbstractController
     private function areUsersDifferents(User $owner, User $beneficiary): bool
     {
         if ($owner instanceof User && $beneficiary instanceof User) {
-            if ($owner != $beneficiary) {
+            if ($owner !== $beneficiary) {
                 return true;
             }
             return false;
         }
     }
 
-    //test is SeedBatch propertiy is set to true and if there is not another donation going on
     private function isBatchAvailable(SeedBatch $seedBatch): bool
     {
         if ($seedBatch instanceof SeedBatch) {
-            if ($seedBatch->isIsAvailable()) {
-                //isAvailable is set to true
-                foreach ($seedBatch->getDonations() as $donation) {
-                    if (
-                        $donation->getStatus() !== Donation::STATUS[0]
-                        && $donation->getStatus() !== Donation::STATUS[1]
-                    ) {
-                        //the is no donations for this batch with a donation over or going on
-                        return true;
-                    }
+            if (!$seedBatch->isIsAvailable()) {
+                //isAvailable is set to false
+                return false;
+            }
+            foreach ($seedBatch->getDonations() as $donation) {
+                if (
+                    $donation->getStatus() === Donation::STATUS[0]
+                    || $donation->getStatus() === Donation::STATUS[1]
+                ) {
+                    //there is already an active donation for this batch
+                    return false;
                 }
             }
+            return true;
         }
-        return false;
     }
 
     #[Route('/add/{id}', name: '_add', requirements: ['id' => '\d+'])]
-    public function addDonation(SeedBatch $seedBatch): Response
-    {
+    public function addDonation(
+        SeedBatch $seedBatch,
+        EntityManagerInterface $entityManager
+    ): Response {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $beneficiary = $this->getUser();
         $owner = $seedBatch->getOwner();
-
-        if ($this->areUsersDifferents($owner, $beneficiary)) {
+        if (!$this->areUsersDifferents($owner, $beneficiary)) {
             //Owner and Beneficiary are different people
-            if ($this->isBatchAvailable($seedBatch)) {
-                //isAvailable is set to true for this batch
-                // and there is no donation going on or over (except of canceled ones)
-                $this->addFlash(
-                    'success',
-                    'Votre demande de graine a bien été enregistrée, le donateur a été prévenu par email'
-                );
-            }
+            $this->addFlash(
+                'danger',
+                'Vous ne pouvez pas réserver votre propre lot'
+            );
+            return $this->redirectToRoute('home');
         }
+        if (!$this->isBatchAvailable($seedBatch)) {
+            //there is a donation going on or over for this batch
+            dd($seedBatch);
+            $this->addFlash(
+                'danger',
+                'Désolé.e, ce lot a déjà été réservé par quelqu\'un d\'autre'
+            );
+            return $this->redirectToRoute('home');
+        }
+        $donation = new Donation();
+        $donation->setBeneficiary($beneficiary);
+        $donation->setStatus(Donation::STATUS[0]);
+        $donation->setCreatedAt(new DateTimeImmutable());
+        $donation->setSeedBatch($seedBatch);
+        $entityManager->persist($donation);
+        $entityManager->flush($donation);
+
+        $this->addFlash(
+            'success',
+            'Votre demande de graine a bien été enregistrée, le donateur a été prévenu par email'
+        );
 
         return $this->redirectToRoute('user_account');
     }
