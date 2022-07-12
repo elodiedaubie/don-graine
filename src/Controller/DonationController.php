506025ebb85042a440a7710039c00693cb570ac5
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-12 15:21:21 +0200

    :sparkles: feat:create private function isBatchAvailable

--- src/Controller/DonationController.php
+++ src/Controller/DonationController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Donation;
 use App\Entity\SeedBatch;
 use App\Entity\User;
-use PhpParser\Node\Stmt\Catch_;
-use PhpParser\Node\Stmt\TryCatch;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,13 +25,25 @@ class DonationController extends AbstractController
         }
     }
 
-    //private function checkBatchAvaibility
-        //test if SeedBatch->isAvailable = true;
-        //test if SeedBatch->getDonation = null :
-            //null = available
-            // notnull = test if status is "annulé") :
-                //true = available
-                //false = add flash: ce lot n'est plus disponible + setISavailable->false
+    //test is SeedBatch propertiy is set to true and if there is not another donation going on
+    private function isBatchAvailable(SeedBatch $seedBatch): bool
+    {
+        if ($seedBatch instanceof SeedBatch) {
+            if ($seedBatch->isIsAvailable()) {
+                //isAvailable is set to true
+                foreach ($seedBatch->getDonations() as $donation) {
+                    if (
+                        $donation->getStatus() !== Donation::STATUS[0]
+                        && $donation->getStatus() !== Donation::STATUS[1]
+                    ) {
+                        //the is no donations for this batch with a donation over or going on
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
     #[Route('/add/{id}', name: '_add', requirements: ['id' => '\d+'])]
     public function addDonation(SeedBatch $seedBatch): Response
@@ -42,15 +53,16 @@ class DonationController extends AbstractController
         $beneficiary = $this->getUser();
         $owner = $seedBatch->getOwner();
 
-        //checkUsersConformity
         if ($this->areUsersDifferents($owner, $beneficiary)) {
-            //checkBatchesAvailability
-            //create a new donation
-            //persist and flush
-            $this->addFlash(
-                'success',
-                'Votre demande de graine a bien été enregistrée, le donateur a été prévenu par email'
-            );
+            //Owner and Beneficiary are different people
+            if ($this->isBatchAvailable($seedBatch)) {
+                //isAvailable is set to true for this batch
+                // and there is no donation going on or over (except of canceled ones)
+                $this->addFlash(
+                    'success',
+                    'Votre demande de graine a bien été enregistrée, le donateur a été prévenu par email'
+                );
+            }
         }
 
         return $this->redirectToRoute('user_account');
