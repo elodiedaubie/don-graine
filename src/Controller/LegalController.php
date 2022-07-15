fbcb819a9de9140ecd7fbcbc245122b86a147629
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-15 15:15:42 +0200

    :lipstick: ui:add path to legale_notice in footer

--- src/Controller/LegalController.php
+++ src/Controller/LegalController.php
@@ -6,9 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/', name: 'legal')]
 class LegalController extends AbstractController
 {
-    #[Route('/cgu', name: 'app_legal')]
+    #[Route('cgu', name: '_notice')]
     public function index(): Response
     {
         return $this->render('legal/notice.html.twig');
