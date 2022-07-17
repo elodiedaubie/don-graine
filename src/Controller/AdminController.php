63078ea9d405438f6850eda61dac36a812813e60
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-17 20:15:38 +0200

    :sparkles: display user informations in view

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        if ($this->getUser() && $this->getUser() instanceof User) {
+            $user = $this->getUser();
+        }
         return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+            'user' => $user
         ]);
     }
 }
