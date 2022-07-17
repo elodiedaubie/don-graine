3abd4d3500a0c081b4b8f62333def3c963da7793
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-17 20:37:32 +0200

    :sparkles: feat:display suer informations

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,13 +11,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'admin')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
         if ($this->getUser() && $this->getUser() instanceof User) {
             $user = $this->getUser();
         }
+
         return $this->render('admin/index.html.twig', [
-            'user' => $user
+            'user' => $user,
+            'users' => $userRepository->findAll()
         ]);
     }
 }
