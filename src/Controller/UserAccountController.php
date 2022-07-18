10e79049c16a1a93e466e9a25be418d97ca021fe
Elodie Daubie <elodie.daubie@gmail.com>
2022-07-18 17:24:28 +0200

    --amend

--- src/Controller/UserAccountController.php
+++ src/Controller/UserAccountController.php
@@ -9,13 +9,11 @@ use App\Repository\DonationRepository;
 use App\Repository\SeedBatchRepository;
 use App\Service\DonationManager;
 use Doctrine\ORM\EntityManagerInterface;
-use Exception;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 #[Route('/mon-compte', name: 'user_account')]
